0be5c214259209c8798e5a6e360cc931a9fea0d0
dirtyredz <dirtyredz@live.com>
2019-05-03 23:04:48 -0500

    Added Error Checking on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,12 +4,40 @@ import * as Colors from '../utils/colors'
 import { Link } from "gatsby"
 import HeadShake from 'react-reveal/HeadShake';
 
-export default class Button extends Component {
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true });
+    // You can also log the error to an error reporting service
+    window.Sentry.configureScope((scope) => {
+      Object.keys(info).forEach(key => {
+        scope.setExtra(key, info[key]);
+      });
+    });
+    window.Sentry.captureException(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <MyButton />;
+    }
+    return this.props.children;
+  }
+}
+export default class Main extends Component {
   constructor() {
     super()
     this.state = {
       counter: 0
     }
+
+    this.handleEnterMouse = this.handleEnterMouse.bind(this)
   }
 
   handleEnterMouse() {
@@ -18,30 +46,40 @@ export default class Button extends Component {
   }
 
   render() {
-    const ButtonComponent = this.props.to ? ButtonLink : MyButton
-    const Index = typeof this.props.index === "number" ? this.props.index : Math.floor(Math.random() * 2)
     return (
-      <HeadShake
-        spy={this.state.counter}
-        appear
-        delay={0}
-        duration={500}
-      >
-        <ButtonWrap>
-          <ButtonComponent
-            onMouseEnter={this.handleEnterMouse.bind(this)}
-            {...this.props}
-            color={Index % 2 ? Colors.Green : Colors.Red}
-            >
-            {this.props.text ? this.props.text : "LEARN MORE"}
-          </ButtonComponent>
-        </ButtonWrap>
-      </HeadShake>
+      <ErrorBoundary>
+        <HeadShake
+          spy={this.state.counter}
+          appear
+          delay={0}
+          duration={500}
+        >
+          <MyButton handleEnter={this.handleEnterMouse} {...this.props} />
+        </HeadShake>
+      </ErrorBoundary>
     )
   }
 }
 
-const MyButton = styled.button`
+class MyButton extends Component {
+  render() {
+    const ButtonComponent = this.props.to ? ButtonLink : TheButton
+    const Index = typeof this.props.index === "number" ? this.props.index : Math.floor(Math.random() * 2)
+    return (
+      <ButtonWrap>
+        <ButtonComponent
+          onMouseEnter={this.props.handleEnter}
+          {...this.props}
+          color={Index % 2 ? Colors.Green : Colors.Red}
+          >
+          {this.props.text ? this.props.text : "LEARN MORE"}
+        </ButtonComponent>
+      </ButtonWrap>
+    )
+  }
+}
+
+const TheButton = styled.button`
   background-color: ${(prop) => prop.color};
   max-width: 180px;
   padding: 10px;
